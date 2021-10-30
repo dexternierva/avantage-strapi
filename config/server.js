200cd2dc5f0db1eafed2029424917ffd47e66038
dexternierva <dexternierva@gmail.com>
2021-10-30 21:36:18 +0800

    Install pg for postgres database in digitalocean

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: true,
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'c2619cf21772bdc784fc239b4a3f669d'),
