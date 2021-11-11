46c947d882b2b6a5d9b16b3b3aa9b8fa4085e992
dexternierva <dexternierva@gmail.com>
2021-11-11 16:46:37 +0800

    Created new secret key for mailjet

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,15 @@
 module.exports = ({ env }) => ({
-  email: {
-    provider: "mailjet",
-    providerOptions: {
-      publicApiKey: env("MAILJET_PUBLIC_KEY"),
-      secretApiKey: env("MAILJET_SECRET_KEY"),
+    email: {
+        provider: "mailjet",
+        providerOptions: {
+          publicApiKey: env("MAILJET_PUBLIC_KEY"),
+          secretApiKey: env("MAILJET_SECRET_KEY"),
+        },
+        settings: {
+          defaultFrom: "d.nierva@educar-consultancy.com",
+          defaultFromName: "AVantage Customer Support",
+          defaultTo: "d.nierva@educar-consultancy.com",
+          defaultToName: "Customer Support Notification",
+        },
     },
-    settings: {
-      defaultFrom: "d.nierva@educar-consultancy.com",
-      defaultFromName: "AVantage Customer Support",
-      defaultTo: "d.nierva@educar-consultancy.com",
-      defaultToName: "Customer Support Notification",
-    },
-  },
 });
