54dc06f12e300f6cc0cb92e55f361564188f9cb4
dexternierva <dexternierva@gmail.com>
2021-11-11 13:17:04 +0800

    Configure plugins.js file for nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,3 @@
-const nodemailerNTLMAuth = require('nodemailer-ntlm-auth');
-
 module.exports = ({ env }) => ({
   email: {
     provider: 'nodemailer',
@@ -8,15 +6,11 @@ module.exports = ({ env }) => ({
       port: env('SMTP_PORT', 587),
       auth: {
         type: 'OAuth2',
-        method: 'NTLM',
         user: env('SMTP_USERNAME'),
         clientId: env('CLIENT_ID'),
         clientSecret: env('CLIENT_SECRET'),
         refreshToken: env('REFRESH_TOKEN'),
         accessToken: env('ACCESS_TOKEN')
-      },
-      customAuth: {
-        NTLM: nodemailerNTLMAuth,
       },
     },
     settings: {
